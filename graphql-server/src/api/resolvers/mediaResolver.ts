7a2c647fa0b9028737cbc9a26743014675489b66
Sohaib_School <sohaibe@metropolia.fi>
2024-01-24 14:43:41 +0200

    grapql added

---
+++ graphql-server/src/api/resolvers/mediaResolver.ts
@@ -0,0 +1,16 @@
+import {fetchAllMedia, fetchMediaById, fetchMediaByTag} from '../models/mediaModel';
+
+export default {
+    Query: {
+        mediaItems: async () => {
+            return await fetchAllMedia();
+        },
+        mediaItem: async (_parent: undefined, args: {media_id: string}) => {
+            const id = Number(args.media_id);
+            return await fetchMediaById(id);
+        },
+        mediaItemsByTag: async (_parent: undefined, args: {tag: string}) => {
+            return await fetchMediaByTag(args.tag);
+        },
+    },
+};
