c819a03238dfc6207a48e20689e58a7e670f7e00
Sohaib_School <sohaibe@metropolia.fi>
2024-01-24 16:12:06 +0200

    commit

--- graphql-server/src/api/resolvers/mediaResolver.ts
+++ graphql-server/src/api/resolvers/mediaResolver.ts
@@ -1,23 +1,55 @@
 import { MediaItem } from '@sharedTypes/DBTypes';
-import {fetchAllMedia, fetchMediaById, fetchMediaByTag, postMedia} from '../models/mediaModel';
+import { fetchAllMedia, fetchMediaById, fetchMediaByTag, postMedia, putMedia, postTagToMedia } from '../models/mediaModel';
 
 export default {
     Query: {
         mediaItems: async () => {
             return await fetchAllMedia();
         },
-        mediaItem: async (_parent: undefined, args: {media_id: string}) => {
+        mediaItem: async (_parent: undefined, args: { media_id: string }) => {
             const id = Number(args.media_id);
             return await fetchMediaById(id);
         },
-        mediaItemsByTag: async (_parent: undefined, args: {tag: string}) => {
+        mediaItemsByTag: async (_parent: undefined, args: { tag: string }) => {
             return await fetchMediaByTag(args.tag);
         },
     },
     Mutation: {
-        createMediaItem: async (_parent: undefined, args: {input: Omit<MediaItem, 'media_id' |'created_at' | 'thumbnail'>},) => {
-        return postMedia(args.input);
+        createMediaItem: async (_parent: undefined, args: { input: Omit<MediaItem, 'media_id' | 'created_at' | 'thumbnail'> },) => {
+            return postMedia(args.input);
         },
-        
+        addTagToMediaItem: async (
+
+            _parent: undefined,
+
+            args: {
+                input: {
+                    media_id: string; tag_name:
+                        string
+                }
+            },
+
+        ) => {
+
+            return await postTagToMedia(
+
+                args.input.tag_name,
+
+                Number(args.input.media_id),
+
+            );
+
+        },
+
+        updateMediaItem: async (
+            _parent: undefined,
+            args: {
+                input: Pick<MediaItem, 'title' | 'description'>;
+                media_id: string;
+            },
+        ) => {
+            return await putMedia(args.input, Number(args.media_id));
+        },
+
     },
 };
